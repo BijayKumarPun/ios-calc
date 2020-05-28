d5d53ba9e23f08d1606761b9a71f19a4f996d22e
bijaykumarpun <bijay.workplace@gmail.com>
2020-05-28 21:58:29 +0545

    Make buttons responsive

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,10 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var label:UILabel!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func display( num :UIButton){
+        
+        switch num.tag {
+        case 1:
+            label.text = "1"
+        case 2:
+            label.text = "2"
+            
+        case 3:
+            label.text = "3"
+            
+        case 4:
+            label.text = "4"
+            
+        case 5:
+            label.text = "5"
+        default:
+            print("None")
+            //
+        }
     }
 
 
